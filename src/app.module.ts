9f54ab396ac41d26abe700d4fe60d082556f8e86
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-06-13 22:47:01 +0200

    Created geolocation module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { GeolocationModule } from './geolocation/geolocation.module';
 
 @Module({
-  imports: [],
+  imports: [GeolocationModule],
   controllers: [],
   providers: [],
 })
