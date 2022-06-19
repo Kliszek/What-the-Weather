e80bdf8856890f41c9159ba68087ce47b5f48200
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-06-19 20:15:42 +0200

    Initialized Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('What the Weather?')
+    .setDescription('A simple weather app written in NestJS')
+    .setVersion('0.0.2')
+    .addTag('Get weather')
+    .addTag('Get weather by city')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('v1/api', app, document);
+
   //I provided hostname, so the request body contains IPv4 and not IPv6
   await app.listen(3000, '0.0.0.0');
 }
