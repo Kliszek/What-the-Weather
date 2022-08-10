a89fea7aa95511a483afeae36339cc9e7a9631bf
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-08-11 00:55:35 +0200

    Add https redirect

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,14 @@ async function bootstrap() {
   });
   app.use(helmet());
 
+  if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`);
+      else next();
+    });
+  }
+
   //Setting up the Swagger OpenAPI
   const config = new DocumentBuilder()
     .setTitle('What the Weather?')
